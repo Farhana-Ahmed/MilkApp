45506c59f3c280c18e409d5594b76936e617cf8d
Farhana K <farhanak@Farhanas-Air.home>
2023-01-24 14:27:27 +0100

    pagination fixed

--- client/src/components/Pagination/PageLink.tsx
+++ client/src/components/Pagination/PageLink.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+// import React, { HTMLProps } from "react";
+// import cn from "classnames";
 
-const PageLink = () => {
+// type Props = HTMLProps<HTMLAnchorElement> & { active?: boolean };
+
+// const PageLink = ({ className, children,active,disabled, ...props }: Props) => {
+//   return (
+//     <a {...props} className={cn("page-link", className, {active, disabled})}>
+//       {children}
+//     </a>
+//   );
+// };
+
+// export default PageLink;
+
+
+import { HTMLProps } from 'react';
+import cn from 'classnames';
+import './PageLink.css';
+
+export type Props = HTMLProps<HTMLAnchorElement> & { active?: boolean };
+
+export default function PageLink({
+  className,
+  active,
+  disabled,
+  children,
+  ...otherProps
+}: Props) {
+  const customClassName = cn('page-link', className, {
+    active,
+    disabled,
+  });
+
+  if (disabled) {
+    return <span className={customClassName}>{children}</span>;
+  }
+
   return (
-    <div>PageLink</div>
-  )
+    <a
+      className={customClassName}
+      aria-current={active ? 'page' : undefined}
+      {...otherProps}
+    >
+      {children}
+    </a>
+  );
 }
-
-export default PageLink
--- client/src/components/ProductsList/ProductsList.tsx
+++ client/src/components/ProductsList/ProductsList.tsx
@@ -1,17 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 
 import useFetch from "../hooks/useFetch";
+
+import Pagination from "react-bootstrap/Pagination";
+
 import Product from "../Product/Product";
 
 const ProductsList = () => {
   const { products } = useFetch();
 
+  const [active, setActive] = useState(1);
+
+  let pages = [];
+  const noOfCardsPerPage = 9;
+  const totalProducts = [];
+  for (let number = 1; number <= products.length; number++) {
+    totalProducts.push(number);
+  }
+
+  let indexOfLastProd = active * noOfCardsPerPage;
+  let indexOfFirstProd = indexOfLastProd - noOfCardsPerPage;
+  const afterslice = products.slice(indexOfFirstProd, indexOfLastProd);
+
+  for (let number = 1; number <= 11; number++) {
+    pages.push(
+      <Pagination.Item
+        key={number}
+        active={number === active}
+        onClick={() => pagination(number)}
+      >
+        {number}
+      </Pagination.Item>
+    );
+  }
+  function pagination(number: number) {
+    indexOfLastProd = number * noOfCardsPerPage;
+    indexOfFirstProd = indexOfLastProd - noOfCardsPerPage;
+    setActive(number);
+  }
   const handleAdd = () => {};
   return (
     <div className="cards-container">
-      {products.map((product) => (
+      {afterslice.map((product) => (
         <Product key={product.id} product={product} handleAdd={handleAdd} />
       ))}
+
+      <div className="container d-flex justify-content-center">
+        <Pagination size="sm">
+          <Pagination.Prev
+            onClick={() => {
+              if (active > 1) {
+                pagination(active - 1);
+              }
+            }}
+          />
+          {pages}
+          <Pagination.Next
+            onClick={() => {
+              if (active < 5) {
+                pagination(active + 1);
+              }
+            }}
+          />
+        </Pagination>
+      </div>
     </div>
   );
 };
