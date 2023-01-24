9f47c57107e353b5e724e39d8013f10507474239
Farhana K <farhanak@Farhanas-Air.home>
2023-01-24 16:04:58 +0100

    server side boilerplate in progress

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,15 @@
-import React, { useState } from 'react';
+import React  from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navigation from './components/Navigation/Navigation';
-import Pagination from './components/Pagination/Pagination';
 
 function App() {
   
   return (
    <Router>
      <Navigation />
-     {/* <Pagination
-        currentPage={currentPage}
-        lastPage={lastPage}
-        maxLength={7}
-        setCurrentPage={setCurrentPage}
-      /> */}
+    
       </Router>
     
   );
